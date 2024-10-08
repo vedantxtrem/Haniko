07537823a4917aa78da693cdf6316049c5588aa9
rajyavardhanbithale <bithale03@gmail.com>
2024-10-09 00:51:56 +0530

    about us

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'hanio.peerduck.com',
         port: '',
-        pathname: '*',
+        pathname: '/wp-content/uploads/**',
       },
       {
         protocol: 'https',
         hostname: 'g-6r95onc1ynb.vusercontent.net',
         port: '',
-        pathname: '*',
+        pathname: '/placeholder.svg/*',
       },
     ],
   },
