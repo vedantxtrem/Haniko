5fd9ebefd767f31caddc6df9bb5f46590be307d9
rajyavardhanbithale <bithale03@gmail.com>
2024-10-12 01:26:21 +0530

    type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,13 +18,33 @@ function page() {
       <HeroSection />
       <div className="flex flex-col gap-10">
         <div className="w-3/4 mx-auto gap-5 flex flex-col md:flex-row">
-          <Types />
-          <Types />
+          <Types
+            imageSrc="https://brokenovenbaking.com/wp-content/uploads/2022/11/honey-bun-cookies-14.jpg"
+            title="Linden Honey"
+            description="Linden honey has a strong and very distinctive aroma. At first sniff…"
+            bgColor="bg-[#ffdfcc]"
+          />
+          <Types
+            imageSrc="https://www.tasteofhome.com/wp-content/uploads/2024/02/GettyImages-1684517871-does-honey-expire-JVedit.jpg"
+            title="Jumun Honey"
+            description="Jamun honey is famous for its antihypoxic activity. A specific dose…"
+            bgColor="bg-[#dfe3ec]"
+          />
         </div>
 
         <div className="w-3/4 mx-auto gap-5 flex flex-col md:flex-row">
-          <Types />
-          <Types />
+          <Types
+            imageSrc="https://img.freepik.com/free-photo/honey-jar-with-wooden-honey-dipper_23-2149442339.jpg"
+            title="Organic Honey"
+            description="Organic honey is highly nutritious honey made by bees that…"
+            bgColor="bg-[#f8f8f8]"
+          />
+          <Types
+            imageSrc="https://cdn.shopify.com/s/files/1/0020/7978/5023/files/a-jar-of-brightland-raw-california-honey.jpg?v=1642822491"
+            title="Flower Honey"
+            description="There are many different varieties of flower honey available…"
+            bgColor="bg-[#fecb41]"
+          />
         </div>
       </div>
       <WhyChooseUs />
--- src/component/Types.tsx
+++ src/component/Types.tsx
@@ -1,12 +1,14 @@
-export default function Types() {
+export default function Types({ imageSrc, title, description, bgColor }:any) {
     return (
-        <div className="shadow-xl w-fit mx-auto rounded-3xl">
-            <div className=" md:w-[40rem] h-[18rem] rounded-3xl flex justify-end mx-auto bg-[#ffdfcc] relative overflow-hidden  shadow-inner">
-                <div className="absolute hidden md:block -left-20">
+        <div className="shadow-xl w-fit mx-auto rounded-[35px]">
+            <div
+                className={`md:w-[40rem] h-[18rem] rounded-[35px] flex justify-end mx-auto ${bgColor} relative overflow-hidden shadow-inner`}
+            >
+                <div className="absolute hidden md:block -left-20 h-full">
                     <img
-                        src="https://brokenovenbaking.com/wp-content/uploads/2022/11/honey-bun-cookies-14.jpg"
-                        alt=""
-                        className="md:w-[20rem] h-auto"
+                        src={imageSrc}
+                        alt={title}
+                        className="md:w-[20rem] h-full object-cover"
                     />
                 </div>
 
@@ -14,18 +16,18 @@ export default function Types() {
                 <div className="flex md:ml-10 flex-col gap-5 justify-evenly items-end w-full">
                     <div className="flex flex-col gap-3 p-5">
                         <span className="text-3xl font-semibold text-slate-900">
-                            Linden Honey
+                            {title}
                         </span>
-                        <span className="text-xl  font-medium text-slate-900">
-                            Linden honey has a strong and very distinctive aroma. At first sniff…
+                        <span className="text-xl font-medium text-slate-900">
+                            {description}
                         </span>
                     </div>
 
-                    <span className="text-xl mb-5  text-slate-800 drop-shadow-xl mx-10 border-b-2 border-slate-800 w-fit border-dotted  font-semibold">
+                    <span className="text-xl mb-5 text-slate-800 drop-shadow-xl mx-10 border-b-2 border-slate-800 w-fit border-dotted font-semibold">
                         Learn More
                     </span>
                 </div>
             </div>
         </div>
-    )
+    );
 }
