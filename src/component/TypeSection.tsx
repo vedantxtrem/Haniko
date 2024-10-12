fbccc8dd17aab761402e2ea35f0e0cdb080ccdc1
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-10-12 15:35:37 +0530

    update

--- src/component/TypeSection.tsx
+++ src/component/TypeSection.tsx
@@ -4,16 +4,41 @@ import TypesCard from "@/component/Types";
 function TypeSection() {
   return (
     <div className="flex flex-col gap-10 px-4 md:px-0 my-10">
-      <div className="text-center mb-5">
-        <h2 className="text-lg font-bold text-amber-500 uppercase tracking-wider mb-2">
-          ABOUT OUR PRODUCTS
-        </h2>
-        <h1 className="text-3xl font-black font-harman tracking-tight text-gray-900 sm:text-4xl md:text-5xl my-4">
-          Types of Honey
-        </h1>
-        <p className="text-lg text-gray-600 mt-2 max-w-xl mx-auto">
-          Take a look at the different types of honey we produce.
-        </p>
+      <img
+        src="/image/hexgon.png"
+        alt="spot"
+        className="absolute w-28 mx-auto md:block hidden"
+      />
+      <img
+        src="/image/vector.png"
+        alt="spot"
+        className="absolute left-[20%] w-28 mx-auto md:block hidden"
+      />
+
+      <div className="text-center mb-5 relative">
+        <div>
+          <h2 className="text-lg font-bold text-amber-500 uppercase tracking-wider mb-2">
+            ABOUT OUR PRODUCTS
+          </h2>
+          <h1 className="text-3xl font-black font-harman tracking-tight text-gray-900 sm:text-4xl md:text-5xl my-4">
+            Types of Honey
+          </h1>
+          <p className="  text-lg text-gray-600 mt-2 max-w-xl mx-auto">
+            Take a look at the different types of honey we produce.
+          </p>
+        </div>
+
+        <img
+          src="/image/hexgon.png"
+          alt="spot"
+          className="absolute left-[70%] top-0 w-28 mx-auto md:block hidden"
+        />
+
+        <div className="absolute right-9 top-1  grid grid-cols-5 gap-2 opacity-20 z-10 animate-honeycomb">
+          {[...Array(25)].map((_, i) => (
+            <div key={i} className="w-3 h-3 bg-amber-400 rounded-full "></div>
+          ))}
+        </div>
       </div>
 
       <div className="relative">
@@ -21,13 +46,13 @@ function TypeSection() {
           className="absolute inset-0 z-0 mx-auto animate-pulse"
           style={{
             backgroundImage: `url('/image/bg.svg')`,
-            backgroundSize: 'contain',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat', 
-            width: '90%',
-            height: '90%', 
-            top: '5%',
-            transform: 'translate(-50%, -50%)', 
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            width: "90%",
+            height: "90%",
+            top: "5%",
+            transform: "translate(-50%, -50%)",
           }}
         ></div>
 
