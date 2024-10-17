1d2e07e78320141563a6471ec135eeb773393678
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-10-18 00:08:23 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,34 @@
-"use client"
+"use client";
 
-import BlogSection from '@/component/Blog'
-import HeroSection from '@/component/Header'
-import AboutUs from '@/component/HomeAbout'
-import Product from '@/component/Productsection'
-import WhyChooseUs from '@/component/WhyChossus'
-import VideoSection from '@/component/VideoSection'
-import React from 'react'
-import ShowCase from "@/component/Products"
-import TypeSection from '@/component/TypeSection'
-import Navbar from "@/component/Navbar"
-import Contact from '@/component/Contact'
+import BlogSection from "@/component/Blog";
+import HeroSection from "@/component/Header";
+import AboutUs from "@/component/HomeAbout";
+import Product from "@/component/Productsection";
+import WhyChooseUs from "@/component/WhyChossus";
+import VideoSection from "@/component/VideoSection";
+import React from "react";
+import ShowCase from "@/component/Products";
+import TypeSection from "@/component/TypeSection";
+import Navbar from "@/component/Navbar";
+import Contact from "@/component/Contact";
 
 function page() {
   //test
   return (
     <>
       <div>
-        {/* <HeroSection /> */}
+        <HeroSection />
         <VideoSection />
-        {/* <WhyChooseUs />
-        <ShowCase /> */}
-{/*        
+        <WhyChooseUs />
+        <ShowCase />
+
         <AboutUs />
         <TypeSection />
         <BlogSection />
-        <Contact /> */}
+        <Contact />
       </div>
     </>
-  )
+  );
 }
 
-export default page   
+export default page;
--- src/component/VideoSection.jsx
+++ src/component/VideoSection.jsx
@@ -8,14 +8,8 @@ const poppins = Poppins({
 
 function VideoSection() {
   return (
-<<<<<<< HEAD
     <main className="w-screen relative min-h-screen">
       <div className="absolute top-0 left-0 w-full h-full">
-=======
-    <section className="flex flex-col md:flex-row h-[80vh] w-full">
-      {/* Video Section */}
-      <div className="w-full md:w-1/2 h-full relative">
->>>>>>> 3cc9bd8 (update)
         <video
           className="w-full h-full object-cover backdrop-blur-xl"
           autoPlay
@@ -27,7 +21,6 @@ function VideoSection() {
           <source src="/image/video.webm" type="video/webm" />
           Your browser does not support the video tag.
         </video>
-<<<<<<< HEAD
         <div className="absolute inset-0  opacity-20"></div>
       </div>
 
@@ -50,44 +43,8 @@ function VideoSection() {
            View Products
           </button>
         </div>
-
-=======
-        <div className="absolute inset-0 bg-black opacity-25"></div>
       </div>
-
-      {/* Content Section */}
-      <div className="w-full md:w-1/2 relative flex flex-col justify-center items-center p-6 md:p-28 text-center md:text-left bg-white">
-        {/* Honeycomb animation */}
-        <div className="absolute top-1/4 right-4 rotate-45 grid grid-cols-5 gap-2 opacity-20 z-0 animate-honeycomb">
-          {[...Array(25)].map((_, i) => (
-            <div
-              key={i}
-              className="w-4 h-4 bg-amber-500 rounded-full"
-            ></div>
-          ))}
-        </div>
-
-        {/* Text Content */}
-        <h1 className="text-4xl font-extrabold text-yellow-800 leading-tight tracking-tight sm:text-5xl lg:text-6xl z-10">
-          Organic Hive
-        </h1>
-        <p className="text-lg md:text-xl text-gray-600 my-4 z-10">
-          We traveled over 5,000 miles to find a light & sweet, 100% organic
-          honey and support the local beekeepers who produce it.
-        </p>
-        <button className="px-8 py-3 bg-yellow-500 text-white font-semibold rounded-full border-2 border-yellow-600 hover:bg-yellow-600 hover:border-yellow-500 transition duration-300 ease-in-out shadow-md hover:shadow-lg transform hover:scale-105 z-10">
-          Learn More
-        </button>
-
-        {/* Honey bottle image */}
-        <img
-          src="/image/honey.png"
-          alt="Honey bottle"
-          className="w-full h-auto max-w-[10rem] absolute -rotate-[30deg] bottom-4 right-4 hidden lg:block"
-        />
->>>>>>> 3cc9bd8 (update)
-      </div>
-    </section>
+    </main>
   );
 }
 
