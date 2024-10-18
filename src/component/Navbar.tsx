dfcb7b4f551bbbb2ad4aba1231bc331e41ebc940
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-10-19 01:40:24 +0530

    update

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,96 +1,124 @@
-'use client'
+"use client";
 
 import { Poppins } from "next/font/google";
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-import { HiOutlineGlobeAlt } from 'react-icons/hi';
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { HiOutlineGlobeAlt } from "react-icons/hi";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { BsCart } from "react-icons/bs";
 
-import "animate.css"
+import "animate.css";
 
 const poppins = Poppins({
-    subsets: ['latin'],
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [blog, setBlog] = useState('#blog');
+  const [blog, setBlog] = useState("#blog");
 
-    const toggleSidebar = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
 
-    useEffect(() => {
-        if (window.location.pathname === '/') {
-            setBlog('#blog');
-        } else {
-            setBlog('/#blog');
-        }
-    }, []);
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      setBlog("#blog");
+    } else {
+      setBlog("/#blog");
+    }
+  }, []);
 
+  const options = [
+    {
+      name: "About Us",
+      icon: <AiOutlineInfoCircle className="font-bold mt-1" />,
+      link: "/about-us",
+    },
+    {
+      name: "Shop",
+      icon: <BsCart className="font-bold mt-1" />,
+      link: "/products",
+    },
+    {
+      name: "Blogs",
+      icon: <HiOutlineGlobeAlt className="font-bold mt-1" />,
+      link: blog,
+    },
+  ];
 
+  return (
+    <>
+      <div className="hidden bg-transparent w-11/12 md:flex flex-row justify-between items-center py-5 mt-2 mx-auto">
+        <Link href={"/"}>
+          <img src="/new/logo.webp" alt="" className="max-w-20" />
+        </Link>
+        <div className={`${poppins.className} flex flex-row gap-5`}>
+          {options.map((option, index) => (
+            <Link key={index} href={option.link}>
+              <div className="flex cursor-pointer text-slate-950 text-xl flex-row gap-2 items-start justify-center font-semibold">
+                <span>{option.name}</span>
+                {option.icon}
+              </div>
+            </Link>
+          ))}
+        </div>
 
+        <Link href={"/contact"}>
+          <button className="bg-gradient-to-r h-12 px-6 from-[#fc6571] to-[#ff934a] text-base font-semibold text-white rounded-full transition duration-300 ease-in-out transform hover:scale-x-110">
+            Contact Us
+          </button>
+        </Link>
+      </div>
 
-    const options = [
-        { name: "About Us", icon: <AiOutlineInfoCircle className="font-bold mt-1" />, link: "/about-us" },
-        { name: "Shop", icon: <BsCart className="font-bold mt-1" />, link: "/products" },
-        { name: "Blogs", icon: <HiOutlineGlobeAlt className="font-bold mt-1" />, link: blog }
-    ];
+      <div className="md:hidden relative bg-transparent  w-11/12 flex flex-row justify-center items-center py-3 mx-auto">
+        <div
+          onClick={toggleSidebar}
+          className="z-[500] absolute top-1/2 left-1 transform -translate-y-1/2"
+        >
+          <GiHamburgerMenu />
+        </div>
+        <img src="/new/logo.webp" alt="" className="max-w-20" />
+      </div>
 
-    return (
-        <>
-            <div className="hidden bg-transparent w-11/12 md:flex flex-row justify-between items-center py-5 mt-2 mx-auto">
-                <img src="/new/logo.webp" alt="" className="max-w-20" />
-                <div className={`${poppins.className} flex flex-row gap-5`}>
-                    {options.map((option, index) => (
-                        <Link key={index} href={option.link}>
-                            <div className="flex cursor-pointer text-slate-950 text-xl flex-row gap-2 items-start justify-center font-semibold">
-                                <span>{option.name}</span>
-                                {option.icon}
-                            </div>
-                        </Link>
-                    ))}
+      {isOpen && (
+        <div
+          className={`animate__animated animate__slideInDown absolute mt-3 w-11/12 left-5 transform -translate-x-1/2  glass-card duration-700 transition-transform z-50 flex flex-col items-center justify-center p-5`}
+        >
+          <div
+            className={`${poppins.className} mx-auto min-w-[70%] flex flex-col justify-center items-center gap-3`}
+          >
+            {options.map((option, index) => (
+              <Link
+                href={option.link}
+                onClick={toggleSidebar}
+                className="z-[100]"
+              >
+                <div
+                  key={index}
+                  className="flex w-full bg-amber-500 py-1.5 rounded-2xl bg-opacity-10 mx-auto text-center items-center justify-center text-slate-950 text-xl gap-2 font-semibold"
+                >
+                  {option.icon}
+                  {option.name}
                 </div>
-
-                <Link href={'/contact'}>
-                    <button className="bg-gradient-to-r h-12 px-6 from-[#fc6571] to-[#ff934a] text-base font-semibold text-white rounded-full transition duration-300 ease-in-out transform hover:scale-x-110">
-                        Contact Us
-                    </button>
-                </Link>
-            </div>
-
-            <div className="md:hidden relative bg-transparent  w-11/12 flex flex-row justify-center items-center py-3 mx-auto">
-                <div onClick={toggleSidebar} className="z-[500] absolute top-1/2 left-1 transform -translate-y-1/2">
-                    <GiHamburgerMenu />
-                </div>
-                <img src="/new/logo.webp" alt="" className="max-w-20" />
-            </div>
-
-            {isOpen && (
-                <div className={`animate__animated animate__slideInDown absolute mt-3 w-11/12 left-5 transform -translate-x-1/2  glass-card duration-700 transition-transform z-50 flex flex-col items-center justify-center p-5`}>
-                    <div className={`${poppins.className} mx-auto min-w-[70%] flex flex-col justify-center items-center gap-3`}>
-                        {options.map((option, index) => (
-                            <Link href={option.link} onClick={toggleSidebar} className="z-[100]">
-                                <div key={index} className="flex w-full bg-amber-500 py-1.5 rounded-2xl bg-opacity-10 mx-auto text-center items-center justify-center text-slate-950 text-xl gap-2 font-semibold">
-                                    {option.icon}
-                                    {option.name}
-                                </div>
-                            </Link >
-                        ))}
-                    </div>
-                    <Link href={'/contact'} className="flex w-full justify-center items-center">
-                        <button className="bg-gradient-to-r h-10 min-w-[70%] mt-5 px-6 from-[#fc6571] to-[#ff934a] text-base font-semibold text-white rounded-full transition duration-300 ease-in-out transform hover:scale-x-110">
-                            Contact Us
-                        </button>
-                    </Link>
-                </div>
-            )}
-        </>
-    );
+              </Link>
+            ))}
+          </div>
+          <Link
+            href={"/contact"}
+            className="flex w-full justify-center items-center"
+          >
+            <button className="bg-gradient-to-r h-10 min-w-[70%] mt-5 px-6 from-[#fc6571] to-[#ff934a] text-base font-semibold text-white rounded-full transition duration-300 ease-in-out transform hover:scale-x-110">
+              Contact Us
+            </button>
+          </Link>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,7 +44,7 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        marquee: 'marquee 60s linear infinite',
+        marquee: 'marquee 40s linear infinite',
       },
     },
   },
