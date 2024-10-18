bb78366f2a39d627c6bf01547d346e910ccdc89e
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-10-19 00:46:50 +0530

    new

---
+++ src/component/Promotion.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Promotion() {
+  return (
+    <div className="overflow-hidden whitespace-nowrap bg-gray-800 text-white py-2">
+    <div className="inline-block animate-marquee text-2xl">
+        CERTIFIED B CORP® 100% TRACEABLE GLYPHOSATE RESIDUE-FREE NON-GMO CERTIFIED B CORP® 100% TRACEABLE GLYPHOSATE RESIDUE-FREE NON-GMO
+    </div>
+    <div className="inline-block animate-marquee text-2xl">
+        CERTIFIED B CORP® 100% TRACEABLE GLYPHOSATE RESIDUE-FREE NON-GMO CERTIFIED B CORP® 100% TRACEABLE GLYPHOSATE RESIDUE-FREE NON-GMO
+    </div>
+</div>
+  )
+}
+
+export default Promotion
