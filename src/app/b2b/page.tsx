b52989a136b402518640beec02316b839a261270
rajyavardhanbithale <bithale03@gmail.com>
2024-10-19 00:55:07 +0530

    nav

--- src/app/b2b/page.tsx
+++ src/app/b2b/page.tsx
@@ -1,9 +1,11 @@
+'use client'
+
 import { Parallax } from 'react-parallax';
 
 
 export default function Page() {
     return (
-        <Parallax blur={10} bgImage="path/to/image.jpg" bgImageAlt="the cat" strength={200}>
+        <Parallax blur={10} bgImage="https://static01.nyt.com/images/2022/12/12/us/12xp-bees1/12xp-bees-02-superJumbo.jpg" bgImageAlt="the cat" strength={200}>
             Content goes here. Parallax height grows with content height.
         </Parallax>
     )
