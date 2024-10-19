77f1a82aa166c9251d9037d974fec15c34305275
rajyavardhanbithale <bithale03@gmail.com>
2024-10-19 16:38:09 +0530

    vec white

--- src/app/b2b/page.tsx
+++ src/app/b2b/page.tsx
@@ -5,8 +5,22 @@ import { Parallax } from 'react-parallax';
 
 export default function Page() {
     return (
-        <Parallax blur={10} bgImage="https://static01.nyt.com/images/2022/12/12/us/12xp-bees1/12xp-bees-02-superJumbo.jpg" bgImageAlt="the cat" strength={200}>
-            Content goes here. Parallax height grows with content height.
-        </Parallax>
+        <>
+            <Parallax
+                blur={5}
+                bgImage="https://static01.nyt.com/images/2022/12/12/us/12xp-bees1/12xp-bees-02-superJumbo.jpg"
+                bgImageAlt="the cat"
+                strength={200}
+                className="h-72"
+            >
+                Content goes here. Parallax height grows with content height.
+            </Parallax>
+
+            <div className="h-72"></div>
+            <div className="h-72"></div>
+            <div className="h-72"></div>
+            <div className="h-72"></div>
+            <div className="h-72"></div>
+        </>
     )
 }
