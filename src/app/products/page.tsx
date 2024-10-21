9608eaa8d9a9df35e31da573b9dcda14aa0f2402
vedant <vedant@ssipmt.com>
2024-10-21 22:10:20 +0530

    new

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -46,10 +46,10 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ name, front, back }) => {
   return (
-    <div className="w-full md:w-fit h-full flex flex-col justify-center items-center p-4">
+    <div className="w-full md:w-[280px] lg:w-[300px] flex flex-col justify-center items-center p-4">
       <div className="w-full text-center text-black relative">
-        <div className="group h-[300px] md:h-[400px] w-[250px] md:w-[300px] [perspective:1000px]">
-          <div className="relative h-full w-full rounded-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+        <div className="group h-[280px] md:h-[350px] lg:h-[400px] w-[200px] md:w-[250px] lg:w-[300px] [perspective:1000px] flex justify-center items-center">
+          <div className="relative h-full w-full  rounded-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
             {/* Front side */}
             <div className="absolute inset-0 h-full w-full rounded-xl [backface-visibility:hidden] flex justify-center items-center">
               <img
@@ -73,18 +73,17 @@ const ProductCard: React.FC<ProductCardProps> = ({ name, front, back }) => {
         </div>
       </div>
 
-      <h2 className="font-bold text-base md:text-xl mt-4 text-black uppercase">
+      <h2 className="font-bold text-sm md:text-xl mt-2 md:mt-4 text-black uppercase">
         {name}
       </h2>
 
       <Link
         href="/contact"
-        className="mt-5 bg-yellow-400 text-black font-bold text-sm md:text-xl px-6 py-2 md:px-10 md:py-3 rounded-3xl hover:bg-white border-2 border-yellow-400 transition-colors duration-300"
+        className="mt-3 md:mt-5 bg-yellow-400 text-black font-bold text-xs md:text-lg px-4 py-1 md:px-8 md:py-2 rounded-3xl hover:bg-white border-2 border-yellow-400 transition-colors duration-300"
       >
         Shop Now
       </Link>
     </div>
-
   );
 };
 
@@ -102,14 +101,14 @@ const ProductPage: React.FC = () => {
   };
 
   useEffect(() => {
-    const interval = setInterval(slideToNext, 2000);
+    const interval = setInterval(slideToNext, 3000);
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="min-h-screen w-screen text-white overflow-x-hidden">
       {/* Carousel Div */}
-      <div className="relative h-[360px] w-full flex justify-center items-center overflow-hidden">
+      <div className="relative h-[200px] md:h-[360px] w-full flex justify-center items-center overflow-hidden">
         <div
           className="absolute h-full w-full flex transition-transform duration-1000"
           style={{
@@ -155,32 +154,21 @@ const ProductPage: React.FC = () => {
           onClick={slideToPrev}
           className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white text-black p-2 rounded-full"
         >
-          &#8592; {/* Left Arrow */}
+          &#8592;
         </button>
         <button
           onClick={slideToNext}
           className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white text-black p-2 rounded-full"
         >
-          &#8594; {/* Right Arrow */}
+          &#8594;
         </button>
       </div>
 
       <Promotion />
 
       <div className="w-full h-full flex flex-col justify-center items-center p-6 md:p-12">
-        <div className="w-full h-full flex flex-col md:flex-row md:flex-wrap justify-center  items-center lg:gap-x-56 lg:gap-y-20 ">
-          {products.slice(0, products.length / 2).map((product) => (
-            <ProductCard
-              key={product.name}
-              name={product.name}
-              front={product.front}
-              back={product.back}
-            />
-          ))}
-        </div>
-
-        <div className="w-full h-full flex flex-col md:flex-row md:flex-wrap justify-center  items-center lg:gap-x-56 lg:gap-y-20 ">
-          {products.slice(products.length / 2, products.length).map((product) => (
+        <div className="w-full h-full flex flex-col md:flex-row md:flex-wrap justify-center items-center gap-8 lg:gap-x-16 lg:gap-y-12">
+          {products.map((product) => (
             <ProductCard
               key={product.name}
               name={product.name}
