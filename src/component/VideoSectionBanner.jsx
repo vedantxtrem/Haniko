017da9df3ecf95b8440684f8e5817c6d4f5337eb
vedant <vedant@ssipmt.com>
2024-10-21 22:23:28 +0530

    fix banner mobile ui

--- src/component/VideoSectionBanner.jsx
+++ src/component/VideoSectionBanner.jsx
@@ -9,7 +9,7 @@ const poppins = Poppins({
 
 function VideoSectionBanner() {
   return (
-    <main className="w-screen relative min-h-screen">
+    <main className="w-screen relative h-[50vh] md:h-[80vh] lg:min-h-screen">
       <div className="absolute top-0 left-0 w-full h-full">
         <video
           className="w-full h-full object-cover backdrop-blur-xl"
