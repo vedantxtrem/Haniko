93028ebc12240f43d25409c1ad90d7e1431098a2
vedant <vedant@ssipmt.com>
2024-10-23 01:14:02 +0530

    fx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        style={{ backgroundColor: "#fffbeb" }}
+        // style={{ backgroundColor: "#fffbeb" }}
       >
           <Navbar />
 
